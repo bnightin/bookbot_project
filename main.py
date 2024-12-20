a1a23e12ea90999626580183b028752f6acdfb92
Benjamin Nightingale <bnightin@uncc.edu>
2024-12-20 16:46:32 -0500

    Finished

---
+++ main.py
@@ -0,0 +1,52 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    chars_dict = count_characters(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def get_book_text(text):
+    #Read the text so we can get the data from it
+    with open('books/frankenstein.txt') as f:
+        return f.read()
+
+#Counts the number of words in the file
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+#Counts the number of times each character appears in the book
+def count_characters(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+main()
